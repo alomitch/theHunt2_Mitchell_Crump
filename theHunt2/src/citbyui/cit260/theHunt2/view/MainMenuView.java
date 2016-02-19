0d376f1850baee2ea4475b6e3f750639159c2a25
AlleciaMitchell <AlleciaMitchell@192.168.0.42>
2016-02-18 19:57:13 -0800

    2/17/16- Team Assignment pages 1-43

--- theHunt2/src/citbyui/cit260/theHunt2/view/MainMenuView.java
+++ theHunt2/src/citbyui/cit260/theHunt2/view/MainMenuView.java
@@ -5,6 +5,8 @@
  */
 package citbyui.cit260.theHunt2.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author AlleciaMitchell
@@ -35,6 +37,85 @@ public class MainMenuView {
        
         } while (selection != 'E'); // while selection is not "Exit"
        
+    }
+
+    private String getInput() {
+         boolean valid = false; // indicates if the name has been retrieved
+       String getInput = null;
+       Scanner keyboard = new Scanner(System.in);   // keyboard input stream
+       
+       while (!valid) { // while a valid name has not been retrieved
+           
+           //propt for the player's name
+           System.out.println("Choose Option From Main Menu");
+          
+           // get the name from the keyboard and trim off the blanks
+          getInput = keyboard.nextLine();
+           getInput = getInput.trim();
+           
+           //if the name is invalid (less than two character in lenght))
+           if (getInput. length() <1){
+               System.out.println("Invalid entry");
+               continue; // and repeat again
+               
+           }
+           break; // out of the (exit) the repetition
+       }
+       return getInput; //return the name
+    }
+
+    private void doAction(char choice) {
+        switch (choice){
+            case 'N'://create and start a new game
+                this.startNewGame();
+                break;
+            case 'G': //get and start and existing game
+                this.startExistingGame();
+                break;
+            case 'H': // display the help menu
+                this.displayHelpMenu();
+                break;
+            case 'S':// save current game
+                this.saveGame();
+                break;
+            case 'M': // Show game map
+                this.displayGameMap();
+                break;
+           case'R':// Return to Main Menu
+               this.returnToMainMenu();
+               break;
+            case 'E':// exit current game
+                return;
+           default:
+               System.out.println("\n*** Invalid selection *** Try again ");
+               break;
+                
+                
+        }
+    }
+
+    private void startNewGame() {
+       System.out.println("***startNewGame function called ***");
+    }
+
+    private void startExistingGame() {
+       System.out.println("*** startExistingGame function called ***");
+    }
+
+    private void displayHelpMenu() {
+        System.out.println("*** displayHelpMenu function called ***");
+    }
+
+    private void saveGame() {
+        System.out.println("*** startExistingGame function called ***");
+    }
+
+    private void displayGameMap() {
+        System.out.println("*** display game map function called ***");
+    }
+
+    private void returnToMainMenu() {
+       System.out.println("***start returnToMainMenu function called***");
     }
     
 }
