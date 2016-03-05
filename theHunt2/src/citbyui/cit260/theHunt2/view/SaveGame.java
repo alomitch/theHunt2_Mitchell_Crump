82e2efaed24891de266e8b0ad0a3ecdc8ec04034
mikec_000 <mikec_000@Roland.PK5001Z>
2016-03-05 13:57:03 -0700

    2/3/16 - Revised code with call functions. 2/9/16 - Control layer classes. 2/11/16 - Committing changes. 2/11/16 - Corrected errors (!=). 2/11/16 - Added first two test cases. 2/11/16 - Added remaining test cases. 2/11/16 - Minor edits. 2/11/16 - More edits to try to clear errors. 2/11/16 - Final edits.  100% pass rate. 2/12/16 - Adding PuzzleSceneWater class and test packages. 2/13/16 - Final edits to PuzzleSceneWater.  100% pass rate. 2/18/16 - MainMenuView additions. 2/19/16 - Added HelpMenuView. 2/19/16 - Minor edits. 2/23/16 - Added Items Menu. 2/24/16 - Attempted Water Puzzle View 03/03/16 - Extended View Interface 03/05/16 - Added SaveGame.java

---
+++ theHunt2/src/citbyui/cit260/theHunt2/view/SaveGame.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citbyui.cit260.theHunt2.view;
+
+/**
+ *
+ * @author mikec_000
+ */
+
+public abstract class SaveGame extends View{
+ 
+        private final String MENU = "\n"
+            +"\n-----------------------------------------"
+            + "\n| Save Game                            |"
+            +"\n-----------------------------------------"
+            +"\nS - Save Game"
+            +"\nR - Return to Main Menu"
+            +"\n-----------------------------------------";
+
+    
+
+    private void doAction(char choice) {
+        switch (choice){
+            case 'S':// save current game
+                this.saveGame();
+                break;     
+            case 'R':// Return to Main Menu
+               this.returnToMainMenu();
+               return;
+            default:
+               System.out.println("\n*** Invalid selection *** Try again ");
+               break;          
+               
+        }
+    }
+
+    private void saveGame() {
+        System.out.println("*** start saveGame function called ***");
+    }
+
+        
+    private void returnToMainMenu() {
+       MainMenuView mainMenu = new MainMenuView();
+       mainMenu.display();
+    }
+
+}
