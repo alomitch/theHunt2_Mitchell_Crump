062e19c988c39b80cf9a4c04f434f9f732893a1f
AlleciaMitchell <AlleciaMitchell@192.168.0.42>
2016-03-10 20:55:36 -0800

    Actor fixed

--- theHunt2/src/byu/cit260/theHunt2/model/Actor.java
+++ theHunt2/src/byu/cit260/theHunt2/model/Actor.java
@@ -3,40 +3,44 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package byu.cit260.theHunt2.model;
-
+import java.awt.Point;
 import java.io.Serializable;
-
 /**
- *
- * @author mikec_000
- */
+*
+* @author mikec_000
+*/
 public enum Actor implements Serializable {
-    
-    Monkey("He is a goofy animal on the island."),
-      
-    private final String description;
-    private final Point coordinates;
 
-    Actor (String description){
-      this.description = description;
-      coordinates = new Point(1,1);
-    }
+Monkey("He is a goofy animal on the island."),
+Dog("4 legged animal");
 
-    public String getDescription() {
-        return description;
-    }
+private final String description;
+private final Point coordinates;
+private String name;
 
-    public Point getCoordinates() {
-        return coordinates;
-    }
-
+Actor (String description){
+this.description = description;
+coordinates = new Point(1,1);
 }
-    @Override
-    public String toString() {
-        return "Actor{" + "name=" + name + ", description=" + description + ", coordinates=" + coordinates + '}';
-    }
-    
-    
-    
+public String getDescription() {
+return description;
+}
+public Point getCoordinates() {
+return coordinates;
+}
+public String getName() {
+return name;
+}
+public void setName(String name) {
+this.name = name;
+}
+@Override
+public String toString() {
+return "Actor{" + "name=" + name + ", description=" + description + ", coordinates=" + coordinates + '}';
+}
+
+
+
 }
