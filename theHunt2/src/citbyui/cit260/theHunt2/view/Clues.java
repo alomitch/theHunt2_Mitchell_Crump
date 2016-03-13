6fd6454fe6bd316852d4d8fddc3dddfb0b5423ca
mikec_000 <mikec_000@Roland.PK5001Z>
2016-03-12 18:51:54 -0700

    2/3/16 - Revised code with call functions. 2/9/16 - Control layer classes. 2/11/16 - Committing changes. 2/11/16 - Corrected errors (!=). 2/11/16 - Added first two test cases. 2/11/16 - Added remaining test cases. 2/11/16 - Minor edits. 2/11/16 - More edits to try to clear errors. 2/11/16 - Final edits.  100% pass rate. 2/12/16 - Adding PuzzleSceneWater class and test packages. 2/13/16 - Final edits to PuzzleSceneWater.  100% pass rate. 2/18/16 - MainMenuView additions. 2/19/16 - Added HelpMenuView. 2/19/16 - Minor edits. 2/23/16 - Added Items Menu. 2/24/16 - Attempted Water Puzzle View 03/03/16 - Extended View Interface 03/05/16 - Added SaveGame.java 03/05/16 - Major code corrections with Brother Jones. 03/07/16 - Minor edits. 3/9/16 - Edits to MainMenuView and HelpMenuView 3/10/16 - Lesson 09 Group Assignment, through page 18. 3/10/16 - Lesson 09 Group Assignment, through page 23. 3/11/16 - Lesson 09 Group Assignment, through page 27. 3/11/16 - Lesson 09 Group Assignment, through page 30. 3/12/16 - Added Clues.java

---
+++ theHunt2/src/citbyui/cit260/theHunt2/view/Clues.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citbyui.cit260.theHunt2.view;
+
+import byu.cit260.theHunt2.control.GameControl;
+import byu.cit260.theHunt2.control.InventoryItem;
+
+/**
+ *
+ * @author mikec_000
+ */
+public abstract class Clues extends View{
+     
+       public Clues(){
+           super("\n"  
+            +"\n-----------------------------------------"
+            +"\n| Clues                                 |"
+            +"\n-----------------------------------------"
+            +"\nW - Water Scene Clue"
+            +"\nH - Half-Gallon Clue"
+            +"\nM - Monkey Clue"
+            +"\nO - Old Cell Phone clue"   
+            +"\nR - Return to Main Menu"
+            +"\n-----------------------------------------");
+       }
+    
+
+       @Override
+     public boolean doAction(String value) {
+        char choice = value.charAt(0);
+        
+        switch (choice){
+            case 'W':// display Water Scene clue
+                this.water();
+                break;
+            case 'H': // display Half Gallon clue
+                this.half();
+                break;
+            case 'M': // display Monkey clue
+                this.monkey();
+                break;
+            case 'O':// display old cell phone clue
+                this.oldCellPhone();
+                break;                   
+            case 'R':// Return to Main Menu
+               this.returnToMainMenu();
+               return true;
+            default:
+               System.out.println("\n*** Invalid selection *** Try again ");
+               break;          
+               
+        }
+        return false;
+    }
+
+    private void water() {
+        System.out.println("*** start water clue function called ***");
+    }
+
+    private void half() {
+       System.out.println("***start half gallon clue function called ***");
+    }
+
+    private void monkey() {
+        System.out.println("***start monkey function called ***");
+    }
+
+    private void oldCellPhone() {
+        System.out.println("*** start oldCellPhone function called ***");
+    }    
+    
+    private void returnToMainMenu() {
+       MainMenuView mainMenu = new MainMenuView();
+       mainMenu.display();
+    }
+
+    private void viewClues() {
+        //get the sorted list of clues acquired
+        InventoryItem[] clue = GameControl.getSortedClueList() ;
+        
+        System.out.println("\nList of clues");
+        System.out.println("Description" + "\t" +
+                            "In Backpack");
+        
+        //for each clue
+        for (InventoryItem inventoryItem : clue) {
+            //DISPLAY the clue and how many in inventory
+            System.out.println(inventoryItem.getDescription() + "\t     " +
+                    inventoryItem.getQuantityInStock());
+        }
+    }
+}
+
