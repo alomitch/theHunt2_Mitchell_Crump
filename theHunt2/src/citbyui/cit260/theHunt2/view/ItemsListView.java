e27c0d59b7f0a7fa5c8e3bca37581cbc120b67ba
mikec_000 <mikec_000@Roland.PK5001Z>
2016-03-26 18:16:34 -0600

    2/3/16 - Revised code with call functions. 2/9/16 - Control layer classes. 2/11/16 - Committing changes. 2/11/16 - Corrected errors (!=). 2/11/16 - Added first two test cases. 2/11/16 - Added remaining test cases. 2/11/16 - Minor edits. 2/11/16 - More edits to try to clear errors. 2/11/16 - Final edits.  100% pass rate. 2/12/16 - Adding PuzzleSceneWater class and test packages. 2/13/16 - Final edits to PuzzleSceneWater.  100% pass rate. 2/18/16 - MainMenuView additions. 2/19/16 - Added HelpMenuView. 2/19/16 - Minor edits. 2/23/16 - Added Items Menu. 2/24/16 - Attempted Water Puzzle View 03/03/16 - Extended View Interface 03/05/16 - Added SaveGame.java 03/05/16 - Major code corrections with Brother Jones. 03/07/16 - Minor edits. 3/9/16 - Edits to MainMenuView and HelpMenuView 3/10/16 - Lesson 09 Group Assignment, through page 18. 3/10/16 - Lesson 09 Group Assignment, through page 23. 3/11/16 - Lesson 09 Group Assignment, through page 27. 3/11/16 - Lesson 09 Group Assignment, through page 30. 3/12/16 - Added Clues.java 3/17/16 - Lesson 10 Group Assignment 3/18/16 - MRC Lesson 10 Individual Assignment 3/23/16 - Group Assignment 3/24/16 - Group assignment, further "System.out" corrections 3/26/16 - MRC Individual Assignment

--- theHunt2/src/citbyui/cit260/theHunt2/view/ItemsListView.java
+++ theHunt2/src/citbyui/cit260/theHunt2/view/ItemsListView.java
@@ -5,11 +5,26 @@
  */
 package citbyui.cit260.theHunt2.view;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * @author mikec_000
  */
 public class ItemsListView {
+
+    public static void main(String[] args) {
+        String string = "Items List";
+        
+        try (BufferedWriter writer = new BufferedWriter (new FileWriter(".\\items.txt"))) {
+            
+            writer.write(string);
+            
+        }catch (IOException e) {
+        }
+    }
 
     void display() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
